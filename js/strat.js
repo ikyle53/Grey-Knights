79b5de5f4228c25b1847ac4e77eb62d1d5bd5e83
Kyle H <ikyle53@icloud.com>
2022-04-04 21:06:10 -0700

    Added first set of strategems

--- js/strat.js
+++ js/strat.js
@@ -69,6 +69,7 @@ function any(name, description) {
 
 //Before Battle ********************************************
 let armory = new before('Armory of Titan', 'Use this strategem before the battle, when you are mustering your army, if your warlord has the grey knights keyword. Select one grey knights character model from your army and give them one relic of titan (this must be a relic they could have.) Each relic in your army must be unique, and you cannot use this strategem to give a model two relics. You can only use this strategem once, unless you are playing a strike force battle or an onslaught battle.');
+let exemplar = new before('Exemplar of the Silvered Host', 'Use this strategem after nominating a grey knights character model that is not a named character to be your warlord. Gnerate one additional warlord trait for them; this must be from the grey knights warlord trats table. Each warlord trait in your army must be unique. You can only use this strategem once.');
 
 
 
@@ -78,6 +79,16 @@ function buildBeforeBattle() {
     h1.textContent = `Before Battle`;
     main.appendChild(h1);
 
-    
+    for (let i = 0; i < beforeBattle.length; i++) {
+        let section = document.createElement('section');
+        main.appendChild(section);
 
+        let h2 = document.createElement('h2');
+        h2.textContent = beforeBattle[i].name;
+        section.appendChild(h2);
+
+        let pd = document.createElement('p');
+        pd.textContent = beforeBattle[i].description;
+        section.appendChild(pd);
+    }
 } buildBeforeBattle();
